640ef44876aa75be8b111c35619fe4bf7a2cdccc
Mykyta Shcheholevatyi Oleksiiovych <mykytashch@hotmail.com>
2024-03-18 20:29:02 +0100

    Update webhook-server.js

--- webhook-server.js
+++ webhook-server.js
@@ -1,21 +1,15 @@
 const http = require('http');
 const { exec } = require('child_process');
-const fs = require('fs');
-const path = require('path');
 const os = require('os');
 const winston = require('winston');
 
 const PORT = 3000;
-
-// Константы и конфигурация
 const projectDir = '/var/www/dairy-monorepo';
 const goAppPath = `${projectDir}/dairy-monolit/main.go`;
 const goLogPath = `${projectDir}/dairy-monolit/logs/backend.log`;
-const gitRepoUrl = 'https://github.com/mykytashch/dairy.monorepo.git';
 const gitBranch = 'main';
 const logCommitMessage = '[Log Update] Automated log commit';
 
-// Настройка логгера Winston
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
@@ -28,11 +22,9 @@ const logger = winston.createLogger({
   ],
 });
 
-// Создание HTTP сервера
 const server = http.createServer((req, res) => {
   if (req.method === 'POST') {
     let body = '';
-
     req.on('data', chunk => {
       body += chunk.toString();
     });
@@ -43,43 +35,53 @@ const server = http.createServer((req, res) => {
         payload = JSON.parse(body);
       } catch (e) {
         logger.error('Error parsing JSON payload from GitHub webhook');
-        res.statusCode = 400; // Bad request
+        res.statusCode = 400;
         res.end('Error parsing payload');
         return;
       }
 
       const lastCommitMessage = (payload.head_commit && payload.head_commit.message) || '';
-
       if (lastCommitMessage.includes(logCommitMessage)) {
         logger.info('Commit for log update detected, skipping code update to avoid loop.');
         res.end('Log update commit detected, skipping.');
         return;
       }
 
-// Обновление кода из репозитория Git
-exec(`cd ${projectDir} && git pull origin ${gitBranch}`, (updateError, updateStdout, updateStderr) => {
-  if (updateError) {
-    if (updateStderr.includes('hint: You have divergent branches')) {
-      // Если обнаружено расхождение веток, отправляем ответ с предложением выбора стратегии
-      const errorMessage = 'Error updating code: Divergent branches detected. Choose resolution strategy.';
-      const options = ['Merge', 'Rebase', 'Fast-Forward Only'];
-      logger.error(errorMessage);
-      res.statusCode = 409; // Conflict
-      res.end(`${errorMessage}\nOptions: ${options.join(', ')}`);
-      return;
-    } else {
-      // Если другая ошибка, вывести сообщение об ошибке
-      logger.error(`Error updating code: ${updateError}`);
-      res.statusCode = 500; // Internal server error
-      res.end(`Error updating code: ${updateError}`);
-      return;
-    }
+      updateCodeAndLogs(res);
+    });
+  } else {
+    res.statusCode = 404;
+    res.end();
   }
+});
 
-  logger.info(`Code updated: ${updateStdout}`);
-  updateStderr && logger.error(`Update stderr: ${updateStderr}`);
+function updateCodeAndLogs(res) {
+  exec(`cd ${projectDir} && git pull origin ${gitBranch}`, (updateError, updateStdout, updateStderr) => {
+    if (updateError) {
+      if (updateStderr.includes('hint: You have divergent branches')) {
+        const errorMessage = 'Error updating code: Divergent branches detected. Choose resolution strategy.';
+        const options = ['Merge', 'Rebase', 'Fast-Forward Only'];
+        logger.error(errorMessage);
+        res.statusCode = 409;
+        res.end(`${errorMessage}\nOptions: ${options.join(', ')}`);
+        return;
+      } else {
+        logger.error(`Error updating code: ${updateError}`);
+        res.statusCode = 500;
+        res.end(`Error updating code: ${updateError}`);
+        return;
+      }
+    }
 
-  // Запуск Go-приложения
+    logger.info(`Code updated: ${updateStdout}`);
+    updateStderr && logger.error(`Update stderr: ${updateStderr}`);
+
+    startGoApp();
+    commitLogs(res);
+  });
+}
+
+function startGoApp() {
   exec(`go run ${goAppPath} > ${goLogPath} 2>&1 &`, (goError, goStdout, goStderr) => {
     if (goError) {
       logger.error(`Go app exec error: ${goError}`);
@@ -88,12 +90,13 @@ exec(`cd ${projectDir} && git pull origin ${gitBranch}`, (updateError, updateStd
     goStdout && logger.info(`Go app stdout: ${goStdout}`);
     goStderr && logger.info(`Go app stderr: ${goStderr}`);
   });
+}
 
-  // Коммит логов в репозиторий Git
+function commitLogs(res) {
   exec(`cd ${projectDir} && git add . && git commit -m "${logCommitMessage}" && git push origin ${gitBranch}`, (logError, logStdout, logStderr) => {
     if (logError) {
       logger.error(`Error committing logs: ${logError}`);
-      res.statusCode = 500; // Internal server error
+      res.statusCode = 500;
       res.end(`Error committing logs: ${logError}`);
       return;
     }
@@ -102,11 +105,8 @@ exec(`cd ${projectDir} && git pull origin ${gitBranch}`, (updateError, updateStd
     logStderr && logger.error(`Log commit stderr: ${logStderr}`);
     res.end('Code updated, Go app started, and logs committed.');
   });
-});
+}
 
-
-
-// Запуск сервера
 server.listen(PORT, () => {
   const timestamp = new Date().toISOString();
   const host = os.hostname();

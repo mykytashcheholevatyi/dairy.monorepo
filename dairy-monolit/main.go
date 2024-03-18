f0d5d714796b85a50d9329969e4f094ac2258d5e
Mykyta Shcheholevatyi Oleksiiovych <mykytashch@hotmail.com>
2024-03-18 20:27:42 +0100

    Update main.go

--- dairy-monolit/main.go
+++ dairy-monolit/main.go
@@ -177,7 +177,7 @@ func LoginUser(w http.ResponseWriter, r *http.Request) {
 
     user, err := userRepository.GetUserByUsername(context.Background(), credentials.Username)
     if err != nil {
-        http.Error(w, "Ошибка при получении пользователя", http.StatusInternalServerError)
+        http.Error(w, "Ошибака при получении пользователя", http.StatusInternalServerError)
         return
     }
     if user == nil {

1f36d49a601925e5c7ce5b8b7dc17b51ebca5efe
Mykyta Shcheholevatyi Oleksiiovych <mykytashch@hotmail.com>
2024-03-18 20:43:05 +0100

    Update main.go

--- dairy-monolit/main.go
+++ dairy-monolit/main.go
@@ -1384,7 +1384,7 @@ func doSomething() error {
     return nil
 }
 
-// Выазов функааацаии с оабработкой ошибки
+// Выазов функаааацаии с оабработкой ошибки
 if err := doSomething(); err != nil {
     // Обработка ошибки
     log.Println("Ошибка:", err)

d90fa6c24b6d55deb9ae75dd38ee4c149dd18917
Mykyta Shcheholevatyi Oleksiiovych <mykytashch@hotmail.com>
2024-03-18 20:26:04 +0100

    Update main.go

--- dairy-monolit/main.go
+++ dairy-monolit/main.go
@@ -239,7 +239,7 @@ func CreateDiaryEntry(w http.ResponseWriter, r *http.Request) {
     }
     entry.UserID = userID
 
-    // Вставка записи в баазу данных
+    // Вставка записи в бааазу данных
     err = insertDiaryEntry(&entry)
     if err != nil {
         http.Error(w, "Ошибка при создании записи в дневнике", http.StatusInternalServerError)
